4d7c857c6b48f583102f9ce112507fd47a1be5e9
Jakub Zięba <jakubzieba7@gmail.com>
2023-03-30 15:59:02 +0200

    InitializePaths()

--- Tydz6_Zad2_PodglądZdjęcia/Form1.Designer.cs
+++ Tydz6_Zad2_PodglądZdjęcia/Form1.Designer.cs
@@ -31,18 +31,20 @@
             this.pbPict = new System.Windows.Forms.PictureBox();
             this.btnPicLoad = new System.Windows.Forms.Button();
             this.btnClearPicBox = new System.Windows.Forms.Button();
+            this.tbPicturePath = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.pbPict)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pbPict
             // 
-            this.pbPict.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.pbPict.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pbPict.BackColor = System.Drawing.Color.White;
-            this.pbPict.Location = new System.Drawing.Point(2, 73);
+            this.pbPict.Location = new System.Drawing.Point(0, 114);
             this.pbPict.Name = "pbPict";
-            this.pbPict.Size = new System.Drawing.Size(319, 253);
+            this.pbPict.Size = new System.Drawing.Size(486, 470);
             this.pbPict.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pbPict.TabIndex = 0;
             this.pbPict.TabStop = false;
@@ -55,6 +57,7 @@
             this.btnPicLoad.TabIndex = 1;
             this.btnPicLoad.Text = "Wczytaj zdjęcie";
             this.btnPicLoad.UseVisualStyleBackColor = true;
+            this.btnPicLoad.Click += new System.EventHandler(this.btnPicLoad_Click);
             // 
             // btnClearPicBox
             // 
@@ -65,12 +68,30 @@
             this.btnClearPicBox.Text = "Wyczyść";
             this.btnClearPicBox.UseVisualStyleBackColor = true;
             // 
+            // tbPicturePath
+            // 
+            this.tbPicturePath.Location = new System.Drawing.Point(14, 19);
+            this.tbPicturePath.Name = "tbPicturePath";
+            this.tbPicturePath.Size = new System.Drawing.Size(377, 20);
+            this.tbPicturePath.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbPicturePath);
+            this.groupBox1.Location = new System.Drawing.Point(13, 55);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(409, 53);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ścieżka pliku";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(322, 326);
+            this.ClientSize = new System.Drawing.Size(485, 586);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnClearPicBox);
             this.Controls.Add(this.btnPicLoad);
             this.Controls.Add(this.pbPict);
@@ -79,6 +100,8 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Podgląd zdjęcia";
             ((System.ComponentModel.ISupportInitialize)(this.pbPict)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -89,6 +112,8 @@
         private System.Windows.Forms.PictureBox pbPict;
         private System.Windows.Forms.Button btnPicLoad;
         private System.Windows.Forms.Button btnClearPicBox;
+        private System.Windows.Forms.TextBox tbPicturePath;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- Tydz6_Zad2_PodglądZdjęcia/Form1.cs
+++ Tydz6_Zad2_PodglądZdjęcia/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,29 @@ namespace Tydz6_Zad2_PodglądZdjęcia
         public Form1()
         {
             InitializeComponent();
+            InitializePaths();
         }
+
+        private void btnPicLoad_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private string _pictureFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "PicturePath.txt");
+        private void InitializePaths()
+        {
+            if (!File.Exists(_pictureFilePath))
+            {
+                File.Create(_pictureFilePath);
+            }
+
+            
+            using (StreamReader readerPicturePath = new StreamReader(_pictureFilePath))
+            {
+                tbPicturePath.Text = readerPicturePath.ReadToEnd();
+                readerPicturePath.Close();
+            }
+        }
+
     }
 }
